7406a96e5ee29caec16e1c1a141476d825655170
lhs0698 <yohs0698@naver.com>
2022-05-30 02:48:34 +0900

    if

--- js/pratice.js
+++ js/pratice.js
@@ -12,11 +12,17 @@ element.onclick = function(event){
 const click = document.querySelector(".click");
 const clicks = document.querySelector(".click_event");
 
-function event_click(event){
-
-    const img = document.querySelector(".upload_img");
-
+function event_click(){
     // alert("picture");
     // console.log("blue");
-    clicks.innerHTML = new Date();
+    // const img = document.querySelector(".upload_img");
+    const time = new Date();
+    const second = time.getSeconds();
+    clicks.innerHTML = second;
+
+    setInterval(event_click,1000); //1초마다 카운트
+
+    if(second > 30){
+        console.log(second);
+    }
 }
