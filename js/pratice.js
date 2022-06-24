002c8a2cd10bb23312e4214e3ff847979c4078e9
lhs0698 <104623873+lhs0698@users.noreply.github.com>
2022-06-24 18:43:11 +0900

    filter

--- js/pratice.js
+++ js/pratice.js
@@ -1,68 +1,89 @@
 let userList = document.querySelector(".userList");
 
-
 let users = [
     {
-        id: '0',
+        id: 0,
         name: '페이커',
-        age: '26'
+        age: 26
     },
     {
-        id: '1',
+        id: 1,
         name: '구마유시',
-        age: '27'
+        age: 27
     },
     {
-        id: '2',
+        id: 2,
         name: '제우스',
-        age: '28'
+        age: 28
     },
     {
-        id: '3',
+        id: 3,
         name: '쇼메이커',
-        age: '29'
+        age: 29
     },
     {
-        id: '4',
+        id: 4,
         name: '쵸비',
-        age: '30'
+        age: 30
     }
 ]
+let div = document.getElementById("id");
 
-let result1 = users.forEach(function (user) {
-    console.log(user);
-    let li = document.createElement('li');
-
-    li.addEventListener('click',function(){
-        let div = document.getElementById("id");
-        // let div2 = div.innerHTML = user.id + user.name + user.age;
-        div.innerHTML = '<div> ID : ' + user.id + '</div>';
+const listUP = () => {
+    userList.innerHTML = '';
+    users.forEach(function (user) {
+        let li = document.createElement('li');
+        
+        li.innerHTML = user.name;
+        userList.appendChild(li);
+        // let userInfo = document.querySelectorAll("li")
+        
+        
+        li.addEventListener('click',function(){
+            // let div2 = div.innerHTML = user.id + user.name + user.age;
+            div.innerHTML = '<div> ID : ' + user.id + '</div>';
+        });
     });
-    li.innerHTML = user.name;
-    userList.appendChild(li);
-    // let userInfo = document.querySelectorAll("li")
-});
+}
 
-let UserID = document.getElementById("useID");
+const listUPver2 = () => {
+    userList.innerHTML = '';
+    let tmpTxt = '';
+
+    users.forEach(function (user) {
+        tmpTxt += `<li class='item'>${user.name} ${user.age}</li>`
+    });
+    userList.innerHTML = tmpTxt;
+}
+listUPver2()
+// const itmes =  document.querySelectorAll('.item');
+// itmes.forEach((item, index) => {
+//     item.addEventListener('click', function(e) {
+//         div.innerHTML = '<div> ID : ' + users[index].name + '</div>';
+//     })
+// })
+const ul = document.querySelector('.userList')
+ul.addEventListener('click', function(e) {
+    // console.log(e.target)
+    // console.log(e.currentTarget) //
+})
+
 let UserName = document.getElementById("userName");
 let UserAge = document.getElementById("userAge");
 let creatBox = document.getElementById("create");
 
 creatBox.addEventListener('click', function () {
-    let newUser_ID = UserID.value;
+    let newUser_ID = users.length;
     let newUser_Name = UserName.value;
     let newUser_Age = UserAge.value;
-    
     let result = [];
     // let obj = {};
-
     const newBe = {
-        id : newUser_ID,
-        name : newUser_Name,
-        age : newUser_Age,
+        id: newUser_ID,
+        name: newUser_Name,
+        age: parseInt(newUser_Age),
     }
     // console.log(newBe);
-
     users.push(newBe);
     // result.push(newUser_ID);
     // result.push(newUser_Name);
@@ -71,107 +92,68 @@ creatBox.addEventListener('click', function () {
     // console.log(result)
 
     // users.push(result);
-    // console.log(users);
+    listUPver2()
+    
+
+    const del = users.filter(resule => resule.name.length < 4)
+    if(del){
+        
+    }
+    console.log(del);
 })
+
+// const resule = users;
+// const del = users.filter(resule => resule.name.length < 4)
+// console.log(del);
+
+let delButton = document.getElementById("delButton");
+const del = users.delete(resule => resule.name.length < 3)
+
+delButton.addEventListener('click',function() {
+    console.log(del)
+    let resule2 = del
+
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // 
-
-console.log(users);
-
-
-
-
- 
-  
-
-
-
-
-
-
-
-
-
-
-
-// let newUser = [
-//     {
-//         id : '5',
-//         name : '오너',
-//         age : '31'
-//     }
-// ]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// console.log(users);
+// listUP()
 // let result2 = users.map(function(user){
 //     console.log(user)
 //     let li = document.createElement('li');
-
 //     li.addEventListener('click',function(){
 //         alert(user.age);    
 //     })
-
 //     li.innerHTML = user.name;
 //     userList.appendChild(li);
 //     // let userInfo = document.querySelectorAll("li")
-
 // })
-
 // console.log(result2)
-
-
-
-// function li_show() {
-//     let li = document.createElement('li');
-//     li.appendChild(document.createTextNode(Users_list[0].name));
-//     User_list.appendChild(li);
-
-//     let li_1 = document.createElement('li');
-//     li_1.appendChild(document.createTextNode(Users_list[1].name));
-//     User_list.appendChild(li_1);
-
-//     let li_2 = document.createElement('li');
-//     li_2.appendChild(document.createTextNode(Users_list[2].name));
-//     User_list.appendChild(li_2);
-
-//     let li_3 = document.createElement('li');
-//     li_3.appendChild(document.createTextNode(Users_list[3].name));
-//     User_list.appendChild(li_3);
-
-//     let li_4 = document.createElement('li');
-//     li_4.appendChild(document.createTextNode(Users_list[4].name));
-//     User_list.appendChild(li_4);
-
-//     let li_5 = document.createElement('li');
-//     li_5.appendChild(document.createTextNode(Users_list[5].name));
-//     User_list.appendChild(li_5);
-
-//     console.log(li)
-//     User_list.addEventListener('click',addItem);
-
-//     function addItem(event) {
-//         alert(Users_list[0].age);
-//     }
-// }
-
 // li_show();
+
